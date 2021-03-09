5e3232519c328094f99414ebf93ce2d393561cdc
Administrator <578281672@qq.com>
2021-03-10 06:47:29 +0800

    2021-3-4

---
+++ numpy-array使用.py
@@ -0,0 +1,12 @@
+#coding：utf-8
+
+import numpy as np
+
+arr = np.array([1,2,3,4,5])
+print(arr)
+
+arr = np.array([[1,2,3,4,5],[7,6,8,9,10]])
+print(arr)
+
+arr = np.array([3,4,5,6,7],ndmin=4)
+print(arr)
